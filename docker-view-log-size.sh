df5c00e797f84b1f1255456c20702d5c692aa401
Nick Nelson <nick@chrnnk.tv>
2024-10-19 12:41:13 -0400

    Update docker-view-log-size.sh

--- docker-view-log-size.sh
+++ docker-view-log-size.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+echo This script will display the size of your docker logs, so you can see if runaway logging is contributing to your docker.img file filling up
 du -ah /var/lib/docker/containers/ | grep -v "/$" | sort -rh | head -60 | grep .log
