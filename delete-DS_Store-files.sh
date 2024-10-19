1ad07efd6eef5f827376e7b1c3860b1fa7b288f3
Nick Nelson <nick@chrnnk.tv>
2024-10-19 12:18:32 -0400

    Update delete-DS_Store-files.sh

--- delete-DS_Store-files.sh
+++ delete-DS_Store-files.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Searching for (and deleting) .DS_Store Files"
+echo "This script will search for and delete all .DS_Store files on your array created by Apple's Finder"
 echo "This may take a awhile"
-find /mnt/user/media -maxdepth 9999 -noleaf -type f -name ".DS_Store" -exec rm "{}" \;
+find /mnt/user -maxdepth 9999 -noleaf -type f -name ".DS_Store" -exec rm "{}" \;
