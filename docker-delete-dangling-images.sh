4a9fb0f117ff33e4532b834d9086124f5331eab5
Nick Nelson <nick@chrnnk.tv>
2024-10-19 12:13:24 -0400

    Update docker-delete-dangling-images.sh

--- docker-delete-dangling-images.sh
+++ docker-delete-dangling-images.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo This simple script will delete any "dangling" images within your docker.img file
 docker rmi $(docker images --quiet --filter "dangling=true")
 echo Finished
 echo if an error shows above, no dangling images were found to delete
