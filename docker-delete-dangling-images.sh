6877c2a2a21519db34f57cf246365969946f11ac
Nick Nelson <nick@chrnnk.tv>
2024-10-19 12:11:44 -0400

    Update docker-delete-dangling-images.sh

--- docker-delete-dangling-images.sh
+++ docker-delete-dangling-images.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker rmi $(docker images --filter "dangling=true")
+docker rmi $(docker images --quiet --filter "dangling=true")
+echo Finished
+echo if an error shows above, no dangling images were found to delete
