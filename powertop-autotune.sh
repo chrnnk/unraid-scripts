5310c53d3dece3245a34bb636e8afb923b97bd9f
Nick Nelson <nick@chrnnk.tv>
2024-11-17 00:27:28 -0500

    Update powertop-autotune.sh
    fix title

--- powertop-autotune.sh
+++ powertop-autotune.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/emhttp/webGui/scripts/notify -i normal -s "powertop --auto-tune" -b -d "$(powertop --auto-tune 2>&1)"
+/usr/local/emhttp/webGui/scripts/notify -i normal -s "powertop-autotune.sh" -b -d "$(powertop --auto-tune 2>&1)"
