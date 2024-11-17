8fe58407ec3a66c422dd6709c4ed13bb08b7dfd5
Nick Nelson <nick@chrnnk.tv>
2024-11-17 01:04:48 -0500

    Update find-empty-folders-on-disks.sh

--- find-empty-folders-on-disks.sh
+++ find-empty-folders-on-disks.sh
@@ -2,4 +2,4 @@
 # FIND:
 /usr/local/emhttp/webGui/scripts/notify -i normal -s "Find folders with no files" -b -d "$(for i in {1..6}; do find /mnt/disk$i/media -type d -empty -print; done 2>&1)"
 # FIND & DELETE:
-# /usr/local/emhttp/webGui/scripts/notify -i normal -s "Folders with no files" -b -d "$(for i in {1..6}; do find /mnt/disk$i/media -type d -empty -delete -print; done 2>&1)"
+# /usr/local/emhttp/webGui/scripts/notify -i normal -s "Find and delete folders with no files" -b -d "$(for i in {1..6}; do find /mnt/disk$i/media -type d -empty -delete -print; done 2>&1)"
