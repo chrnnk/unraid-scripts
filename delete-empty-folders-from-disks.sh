154d1615e0ae9438f0c21420156ce24f67a62b74
Nick Nelson <nick@chrnnk.tv>
2024-11-17 00:45:41 -0500

    Update delete-empty-folders-from-disks.sh
    add notification agent

--- delete-empty-folders-from-disks.sh
+++ delete-empty-folders-from-disks.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # FIND:
-for i in {1..6};   do find /mnt/disk$i/media -type d -empty -print; done
+/usr/local/emhttp/webGui/scripts/notify -i normal -s "Folders with no files" -b -d "$(for i in {1..6};   do find /mnt/disk$i/media -type d -empty -print; done 2>&1)"
 # FIND & DELETE:
 # for i in {1..6};   do find /mnt/disk$i/media -type d -empty -delete; done
