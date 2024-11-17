e50a28dbc68eed7e54cf049e195f450c80b3fb41
Nick Nelson <nick@chrnnk.tv>
2024-11-17 00:27:08 -0500

    Update var-log-increase-size.sh
    fix title

--- var-log-increase-size.sh
+++ var-log-increase-size.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/local/emhttp/webGui/scripts/notify -i normal -s "powertop --auto-tune (startup)" -b -d "$(mount -o remount,size=512m /var/log 2>&1)"
+/usr/local/emhttp/webGui/scripts/notify -i normal -s "var-log-increase-siz.sh (startup)" -b -d "$(mount -o remount,size=512m /var/log 2>&1)"
